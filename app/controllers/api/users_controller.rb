a275750a2cb9853c10c10a6360d09c06bbc8b916
yihwan <yihwankim@gmail.com>
2018-04-03 14:49:09 -0700

    finished user config

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,24 @@
+class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+      .permit(
+        :first_name,
+        :last_name,
+        :email,
+        :buying_power,
+        :password
+      )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root "static_pages#root"
 
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resource :user, only: [:create]
+    resource :session, only: [:create, :destroy]
+  end
 end
