95ce97d59ed57bbbcfba9b4e3614af54b8ffe1a7
yihwan <yihwankim@gmail.com>
2018-04-05 00:22:11 -0700

    fixed search hover issue

--- frontend/components/header/asset_search/asset_search.jsx
+++ frontend/components/header/asset_search/asset_search.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export const AssetSearch = () => (
   <section className='asset-search-module'>
-    <div className='asset-search-icon'></div>
+    <div className='asset-search-icon'>IC</div>
     <input
       className="asset-search-input"
       type="text"
