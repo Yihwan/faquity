e48cb845d80b5d21032c0ecbbe623dca7ebe8d92
yihwan <yihwankim@gmail.com>
2018-04-28 18:49:11 -0700

    switched buy side-bar

--- frontend/components/main/asset_detail/trade_sidebar/buy_form_container.jsx
+++ frontend/components/main/asset_detail/trade_sidebar/buy_form_container.jsx
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import TradeForm from './trade_form2';
+import TradeForm from './trade_form';
 import { createFill, receiveErrors } from '../../../../actions/fill_actions';
 import { fetchLatestPrice } from '../../../../actions/iex_actions';
 import { currencyFormatter } from '../../../../utils/helpers';
